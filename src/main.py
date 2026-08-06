a00593255253a048fb1332404eb195c6d0dbbd3b
n-rick <e.ficadiere@gmail.com>
2026-08-06 17:53:52 +0200

    feat(database): implement translation memory

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from scanner.scanner import Scanner
 from config.config import Config
+from database.database import Database
 
 
 def main() -> None:
@@ -8,10 +9,20 @@ def main() -> None:
     units = scanner.scan(Config.OSTRANAUTS_DATA_PATH)
 
     print(f"Files scanned : {scanner.scanned_files}")
-    print(f"Text units : {len(units)}")
+    print(f"Text units    : {len(units)}")
+
+    database = Database(Config.DATABASE_PATH)
+    print(f"Database path : {Config.DATABASE_PATH}")
+    database.load()
 
     for unit in units:
-        print(unit)
+        if not database.contains(unit.uid):
+            unit.translated_text = unit.source_text  # Temporaire
+            database.add(unit)
+
+    database.save()
+
+    print(f"Database entries : {len(database.memory['translations'])}")
 
 
 if __name__ == "__main__":
--- src/models/TranslationStatus.py
+++
@@ -1,5 +0,0 @@
-class TranslationStatus(Enum):
-    NEW = "NEW"
-    VALIDATED = "VALIDATED"
-    MODIFIED = "MODIFIED"
-    SKIPPED = "SKIPPED"
